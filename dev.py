82c6741f9e9d245484ea4e61632f8db9eb4ebe0c
kcelia <celia.kherfallah@zama.ai>
2024-01-19 15:36:24 +0100

    chore: update

--- dev.py
+++ dev.py
@@ -1,3 +1,5 @@
+"""Generating deployment files."""
+
 import shutil
 
 from pathlib import Path
@@ -7,7 +9,8 @@ import pandas as pd
 from concrete.ml.sklearn import LogisticRegression as ConcreteLogisticRegression
 from concrete.ml.deployment import FHEModelDev
 
-# Files location
+
+# Data files location
 TRAINING_FILE_NAME = "./data/Training_preprocessed.csv"
 TESTING_FILE_NAME = "./data/Testing_preprocessed.csv"
 
@@ -15,8 +18,6 @@ TESTING_FILE_NAME = "./data/Testing_preprocessed.csv"
 df_train = pd.read_csv(TRAINING_FILE_NAME)
 df_test = pd.read_csv(TESTING_FILE_NAME)
 
-print(df_train.shape)
-print(df_train.columns)
 # Split the data into X_train, y_train, X_test_, y_test sets
 TARGET_COLUMN = ["prognosis_encoded", "prognosis"]
 
@@ -26,14 +27,17 @@ y_test = df_test[TARGET_COLUMN[0]].values.flatten()
 X_train = df_train.drop(TARGET_COLUMN, axis=1)
 X_test = df_test.drop(TARGET_COLUMN, axis=1)
 
+# Concrete ML model
+
 # Models parameters
 optimal_param = {"C": 0.9, "n_bits": 13, "solver": "sag", "multi_class": "auto"}
 
-# Concrete ML model
 clf = ConcreteLogisticRegression(**optimal_param)
 
+# Fit the model
 clf.fit(X_train, y_train)
 
+# Compile the model
 fhe_circuit = clf.compile(X_train)
 
 fhe_circuit.client.keygen(force=False)
